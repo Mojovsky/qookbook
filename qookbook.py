0b3b4206727a78d8479e3ba62417ed1615e1997c
mojovsky <danielderewonko@gmail.com>
2024-04-13 20:28:48 +0100

    implement routes

--- qookbook.py
+++ qookbook.py
@@ -8,9 +8,30 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('index.html')
+
+@app.route('/createprofile')
+def createprofile():
+    return render_template('createprofile.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/profile')
+def profile():
     user_interaction = UserInteraction()
     user, recipes = user_interaction.login("test", "test")
-    return render_template('index.html', title='Home', user=user, recipes=recipes)
+    return render_template('profile.html', title='Profile', user=user, recipes=recipes)
+
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+
 
 
 if __name__ == '__main__':
