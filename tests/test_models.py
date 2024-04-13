cce1856a5ff1a6b70f57cb1368c06b66c377cf5c
mojovsky <danielderewonko@gmail.com>
2024-04-13 18:42:20 +0100

    move into tests folder

---
+++ tests/test_models.py
@@ -0,0 +1,32 @@
+import unittest
+from models import User
+
+class TestUser(unittest.TestCase):
+    def test_name_validation(self):
+        with self.assertRaises(ValueError):
+            user = User('', 'test@test.com', 'password1')
+        with self.assertRaises(ValueError):
+            user = User('ab', 'test@test.com', 'password1')
+        with self.assertRaises(ValueError):
+            user = User('abcdefghijk', 'test@test.com', 'password1')
+
+    def test_email_validation(self):
+        with self.assertRaises(ValueError):
+            user = User('name', 'invalid_email', 'password1')
+
+    def test_password_validation(self):
+        with self.assertRaises(ValueError):
+            user = User('name', 'test@test.com', 'pwd')
+
+    def test_verify_password_true(self):
+        user = User('name', 'test@test.com', 'password1')
+        user.password = user.hash_password('password1')
+        self.assertTrue(user.verify_password('password1'))
+        
+    def test_verify_password_false(self):
+        user = User('name', 'test@test.com', 'password1')
+        user.password = user.hash_password('password1')
+        self.assertFalse(user.verify_password('2password'))
+
+if __name__ == '__main__':
+    unittest.main()
