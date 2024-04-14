4fff5abc6870c9f8697eb0fe3823f60aae95aa77
mojovsky <danielderewonko@gmail.com>
2024-04-14 17:22:20 +0100

    write functionality for signup, search and add fav. add fav not fully functional

--- qookbook.py
+++ qookbook.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from models import UserInteraction
 
@@ -12,11 +12,23 @@ login_manager.init_app(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
 
-@app.route('/createprofile')
-def createprofile():
-    return render_template('createprofile.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    user_interaction = UserInteraction()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password != confirm_password:
+            return "Passwords do not match"
+        else:
+            user = user_interaction.create_user(username, password)
+            login_user(user, remember=True)
+            return redirect(url_for('profile'))
+    return render_template('signup.html', title='Sign Up')
 
 
 @login_manager.user_loader
@@ -35,7 +47,7 @@ def login():
         if user:
             login_user(user, remember=True)
             return redirect(url_for('profile'))
-    return render_template('login.html')
+    return render_template('login.html', title='Login')
 
 
 @app.route('/logout')
@@ -54,10 +66,27 @@ def profile():
     return render_template('profile.html', title='Profile', user=user, recipes=recipes)
 
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    return render_template('search.html')
+    user_interaction = UserInteraction()
+    last_search = user_interaction.temp_recipe_manipulation.read_file()
+    if request.method == 'POST':
+        ingridients = request.form['search']
+        recipes = user_interaction.search_recipes(ingridients)
+        session['recipes'] = recipes
+        return render_template('search.html', title='Search', recipes=recipes, last_search=last_search)
+    return render_template('search.html', title='Search', last_search=last_search)
 
+
+@app.route('/add_to_favorites', methods=['POST'])
+@login_required
+def add_to_favorites():
+    user_interaction = UserInteraction()
+    recipe_title = request.form['recipe_title']
+    recipe = user_interaction.temp_recipe_manipulation.get_recipe_object(recipe_title)
+    user_interaction.recipe_manipulation.add_fav_recipe(current_user.id, recipe)
+    recipes = session.get('recipes')
+    return render_template('search.html', title='Search', recipes=recipes)
 
 
 
